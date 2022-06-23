e4e591527a6d1bd707d03b46ee3d7b57f267d419
Ruben de Roos <deroosruben@gmail.com>
2022-06-23 22:35:51 +0200

    add config

--- src/main/java/nl/thedutchruben/specialarrows/Specialarrows.java
+++ src/main/java/nl/thedutchruben/specialarrows/Specialarrows.java
@@ -4,10 +4,12 @@ import nl.thedutchruben.mccore.Mccore;
 import nl.thedutchruben.mccore.commands.CommandRegistry;
 import nl.thedutchruben.mccore.commands.TabComplete;
 import nl.thedutchruben.mccore.config.UpdateCheckerConfig;
+import nl.thedutchruben.mccore.utils.config.FileManager;
 import nl.thedutchruben.specialarrows.arrows.SpecialArrow;
 import nl.thedutchruben.specialarrows.arrows.types.*;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -18,9 +20,24 @@ import java.util.Set;
 public final class Specialarrows extends JavaPlugin {
     private List<SpecialArrow> arrows = new ArrayList<>();
     private static Specialarrows instance;
+    private FileManager fileManager = new FileManager(this);
     @Override
     public void onEnable() {
         instance = this;
+        FileManager.Config config = fileManager.getConfig("config.yml");
+        FileConfiguration configfileConfiguration = config.get();
+        configfileConfiguration.options().header("TDR Playtime Plugin " +
+                "\nhttps://www.spigotmc.org/resources/tdrplaytime.47894/ \n" +
+                "Change the language to one of the other files default it has nl_NL.yml and en_GB.yml, " +
+                "you can create your own language file");
+        configfileConfiguration.addDefault("settings.update_check", true);
+        configfileConfiguration.addDefault("settings.update_checktime", 20*60*5);
+        configfileConfiguration.addDefault("arrows.explosionarrow.blockDamage", true);
+        configfileConfiguration.addDefault("arrows.explosionarrow.fire", true);
+        configfileConfiguration.addDefault("arrows.explosionarrow.range", 5);
+        configfileConfiguration.addDefault("arrows.snowballarrow.removeTime", 20*5);
+
+        config.copyDefaults(true).save();
         // Plugin startup logic
         Mccore mccore = new Mccore(this,"specialarrows","62adf3511e02c441a5ce2b1a");
         arrows.add(new ExplosionArrow());
